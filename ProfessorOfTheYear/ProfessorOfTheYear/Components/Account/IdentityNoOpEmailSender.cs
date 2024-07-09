624946dc38be5fc8233052b4d5034acac356becf
AleksRiffel <avr-tum@mail.ru>
2024-07-09 17:47:25 +0500

    rusification for code in auth

--- ProfessorOfTheYear/ProfessorOfTheYear/Components/Account/IdentityNoOpEmailSender.cs
+++ ProfessorOfTheYear/ProfessorOfTheYear/Components/Account/IdentityNoOpEmailSender.cs
@@ -10,12 +10,12 @@ namespace ProfessorOfTheYear.Components.Account
         private readonly IEmailSender emailSender = new NoOpEmailSender();
 
         public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink) =>
-            emailSender.SendEmailAsync(email, "Confirm your email", $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.");
+            emailSender.SendEmailAsync(email, "Подтвердите адрес электронной почты", $"<a href='{confirmationLink}'>Нажмите здесь</a>, чтобы подтвердить акаунт.");
 
         public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink) =>
-            emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
+            emailSender.SendEmailAsync(email, "Сбросте пароль", $"<a href='{resetLink}'>Нажмите здесь</a>, чтобы сбросить пароль.");
 
         public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode) =>
-            emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password using the following code: {resetCode}");
+            emailSender.SendEmailAsync(email, "Сбросте пароль", $"Сбросте пароль используя следующий код: {resetCode}");
     }
 }
