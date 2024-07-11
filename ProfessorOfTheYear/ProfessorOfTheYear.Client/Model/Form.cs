766e8e7dc4aad0a9182c0086f014d506aaff8d17
Voldisoriginal <1voldis1@gmail.com>
2024-07-11 09:24:42 +0500

    add mainforms

---
+++ ProfessorOfTheYear/ProfessorOfTheYear.Client/Model/Form.cs
@@ -0,0 +1,15 @@
+﻿namespace ProfessorOfTheYear.Client.Model
+{
+    public class Form
+    {
+        public int selectionOfCompetition { get; set; } = 0;
+        public int selectionOfCategory { get; set; } = 0;
+        public string WorkName { get; set; } = string.Empty;
+        public string PlaceOfWork {  get; set; } = string.Empty;
+        public string Info {  get; set; } = string.Empty;
+        public string FullName {  get; set; } = string.Empty;
+        public DateOnly Birthday { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+        public string Post { get; set; } = string.Empty;
+        public string Experience { get; set; } = string.Empty;
+    }
+}
