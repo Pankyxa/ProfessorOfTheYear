3bdde37007bf26730a222bc7082664de83b77a81
sallymud <nepogodina04@mail.ru>
2024-07-14 20:42:34 +0500

    add application pages with dto (need to fix) + layout, some fixes

---
+++ ProfessorOfTheYear/ProfessorOfTheYear.Client/Data/MockDataTeaching.cs
@@ -0,0 +1,21 @@
+﻿namespace ProfessorOfTheYear.Client.Data
+{
+    public class MockDataTeaching: IDataServiceTeaching
+    {
+        private static readonly List<DataTeachingDTO.BaseInformation> baseInformation=[new DataTeachingDTO.BaseInformation()];
+
+        public async Task UpdateAsync(DataTeachingDTO.BaseInformation taskItem)
+        {
+            await Task.Run(() =>
+            {
+                taskItem.ID = baseInformation.Max(t => t.ID) + 1;
+                baseInformation.Add(taskItem);
+            });
+        }
+
+        public async Task<List<DataTeachingDTO.BaseInformation>> GetAllAsync()
+        {
+            return await Task.FromResult<List<DataTeachingDTO.BaseInformation>>(baseInformation);
+        }
+    }
+}
--- ProfessorOfTheYear/ProfessorOfTheYear.Client/Program.cs
+++ ProfessorOfTheYear/ProfessorOfTheYear.Client/Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProfessorOfTheYear.Client;
+using ProfessorOfTheYear.Client.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+builder.Services.AddScoped<IDataServiceTeaching, MockDataTeaching>();
 
 builder.Services.AddBlazorBootstrap();
 
